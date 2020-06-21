f871bfdc4a3127a131684d3d41efd20ee3b58767
Kyle Bartolucci <bartoluccik@gmail.com>
2020-06-20 22:58:50 -0700

    Updates

---
+++ precache-manifest.b393c674f1e0184316919ee13c1c25d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd33f8a43b7ae1e8f6e1eb4bdca8c4d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "29b5557ec3aa86b3011b",
+    "url": "/static/css/main.2621ec49.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b5557ec3aa86b3011b",
+    "url": "/static/js/main.186b856e.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
