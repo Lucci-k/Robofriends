3d9d7bf226653c795965769147126da36e089c2b
BartolucciK <bartoluccik@gmail.com>
2020-08-01 23:28:47 -0700

    Updates

---
+++ precache-manifest.0442c2a13cdbe95f7cbd217101677806.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62902b0b0161f76b2efda934f306d3c6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "822f06b283cdb8415c6b",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "eb58b5410bdc28f5ce2f",
+    "url": "/robofriends/static/css/main.85b55515.chunk.css"
+  },
+  {
+    "revision": "822f06b283cdb8415c6b",
+    "url": "/robofriends/static/js/2.b2278dc9.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/robofriends/static/js/2.b2278dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856b48b7940f3c011a29",
+    "url": "/robofriends/static/js/3.dd02df56.chunk.js"
+  },
+  {
+    "revision": "eb58b5410bdc28f5ce2f",
+    "url": "/robofriends/static/js/main.7d92409e.chunk.js"
+  },
+  {
+    "revision": "f1ae19e9922cbd852767",
+    "url": "/robofriends/static/js/runtime-main.c982f943.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Lucci-k/Robofriends/precache-manifest.e7b496f2d2514d2fc31084bbe41cba4d.js"
+  "/robofriends/precache-manifest.0442c2a13cdbe95f7cbd217101677806.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Lucci-k/Robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
